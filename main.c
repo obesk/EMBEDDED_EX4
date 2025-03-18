c65f1ef135935de3d2a2d75259e24b72ee5fb98c
Elisa <s6504193@studenti.unige.it>
2025-03-18 11:09:24 +0100

    Reading working; writing not working

--- main.c
+++ main.c
@@ -9,28 +9,28 @@
 #include "xc.h"
 
 int main(void) {
+    // ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
+    // TRISG = 0x0000;
     
-    RPINR18.U1RXR = 0b1001011; // mapping pin RD11(RPI75) to UART RX
-    RPOR0.RP64R = 0b000001; //mapping pin RD0(RP64) to UART TX
+    RPINR18bits.U1RXR = 0b1001011; // mapping pin RD11(RPI75) to UART RX
+    RPOR0bits.RP64R = 0b000001; // mapping pin RD0(RP64) to UART TX
     
-    U1BRG = 467; // baud rate to 9600 -> 72 000 000 / (16 ? 9600) ? 1 
-
+    U1BRG = 467; // baud rate to 9600 -> 72 000 000 / (16 * 9600) - 1 
 
     U1STAbits.URXISEL = 0; // interrupt on char received
     U1MODEbits.UARTEN = 1; // enable UART 
-    IEC0bits.U1RXIE = 1;
     
+    IFS0bits.U1RXIF = 0; // interrupt flag set to 0
+    INTCON2bits.GIE = 1; // global enabling 
+    IEC0bits.U1RXIE = 1; // interrupt enabled
     
+    while(1);
     
-    
-        
     return 0;
 }
 
 void __attribute__((__interrupt__)) _U1RXInterrupt(void) { 
     IFS0bits.U1RXIF = 0; 
-    U1RXREG;
-    
-
-    
+    U1TXREG = 'a'; 
+    // LATGbits.LATG9 = 1;
 }
