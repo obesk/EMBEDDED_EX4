23611042a01668d6ebb09c4f950a44b696a84ca8
Roberto Bertelli <robbybertelli@gmail.com>
2025-03-26 15:22:17 +0100

    Finished advanced exercise
    bugfix in character reading and implemented data printing
    
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 // 100Hz (the while frequency) / 2.5Hz (the LD2 frequency)
 #define CLOCK_LD_TOGGLE 40 
+#define MAX_INT_LEN 2
 
 int ld2_blink = 0;
 int UART_chars_n = 0;
@@ -19,11 +20,24 @@ void algorithm() {
     tmr_wait_ms(TIMER2, 7);
 }
 
+
+
 int main(void) {
     // enabling the led output
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
-    TRISA = 0x0000;
-    TRISG = 0x0000;
+    TRISA = 0;
+    TRISG = 0;
+    
+    RPINR0bits.INT1R = 0x58; // remapping the interrupt 1 to the T2 button pin
+    IFS1bits.INT1IF = 0; // resetting flag of interrupt 1
+    
+    RPINR1bits.INT2R = 0x59; // remapping the interrupt 2 to the T3 button pin
+    IFS1bits.INT2IF = 0; // resetting flag of interrupt 2
+    
+    INTCON2bits.GIE = 1; // global interrupt enable
+    IEC1bits.INT1IE = 1; // enabling interrupt 1
+    IEC1bits.INT2IE = 1; // enabling interrupt 2
+    
     
     RPINR18bits.U1RXR = 0b1001011; // mapping pin RD11(RPI75) to UART RX
     RPOR0bits.RP64R = 0b000001; // mapping pin RD0(RP64) to UART TX
@@ -58,12 +72,16 @@ int str_crs = 0;
 
 void __attribute__((__interrupt__)) _U1RXInterrupt(void) {
     IFS0bits.U1RXIF = 0; //resetting the interrupt flag to 0
+    
+    ++UART_chars_n; 
 
     static const char *LD1 = "LD1";
     static const char *LD2 = "LD2";
     
-    U1TXREG = str_crs;
-    const char read_char = (char) U1RXREG;
+    //U1TXREG = U1RXREG;
+    const char read_char = U1RXREG;
+    
+    U1TXREG = read_char;
 
     if (read_char == LD1[str_crs] || read_char == LD2[str_crs]) {
         ++str_crs;
@@ -80,4 +98,46 @@ void __attribute__((__interrupt__)) _U1RXInterrupt(void) {
     } else {
         str_crs = 0;
     }
+
+}
+
+void print_int_to_uart(int v) {
+    static char rev_str [MAX_INT_LEN];
+    int crs = 0;
+    
+    if (!v) {
+        U1TXREG = '0';
+    }
+    
+    if (v < 0) {
+        v = -v;
+        U1TXREG = '-';
+        ++crs;
+    }
+    
+    
+    while (v) {
+        rev_str[crs++] = '0' + (v % 10);
+        v /= 10;
+    }
+    
+    for (int i = crs - 1; i >= 0; --i) {
+        U1TXREG = rev_str[i];
+    }
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _INT1Interrupt(){
+    IFS1bits.INT1IF = 0;
+    U1TXREG = '\n';
+    U1TXREG = 'C';
+    U1TXREG = '=';
+    print_int_to_uart(UART_chars_n);
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _INT2Interrupt(){
+    IFS1bits.INT2IF = 0;
+    U1TXREG = '\n';
+    U1TXREG = 'D';
+    U1TXREG = '=';
+    print_int_to_uart(missed_deadlines);
 }
